6246cff171aa52ff2c552472cac7616b9985f57a
Carolina <carolina.zunigac.23@gmail.com>
2023-04-21 20:53:56 -0400

    creacion navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,7 +1,18 @@
 import React from "react";
 
 const Navbar = () => {
-	return <div>Navbar</div>;
+	return ( 
+	<nav class="navbar">
+		<div>
+			<a href="#nosotros">
+				<h1 class="nosotros">|Nosotros|</h1>
+			</a>
+			<a href="#femit">
+				<h1 class="femit">|FemIT|</h1>
+			</a>
+		</div>
+	</nav>
+	);
 };
 
 export default Navbar;
